f9fb26e6e71af66f9da1ea1b02298331622895de
catley85 <chriscatley85@gmail.com>
2017-08-14 18:01:23 +0100

    Moved feature functionality into feature files

--- src/js/controllers/main-controllers/featured.controller.js
+++ src/js/controllers/main-controllers/featured.controller.js
@@ -2,26 +2,36 @@ angular
 .module('WDI_Group_Project')
 .controller('FeaturedCtrl', FeaturedCtrl);
 
-FeaturedCtrl.$inject=['$http', 'filterFilter', '$scope'];
-function FeaturedCtrl($http, filterFilter, $scope) {
-  console.log('Hello');
+FeaturedCtrl.$inject = ['Allotments','filterFilter', '$scope', '$http'];
 
-  $http
-  .get('http://api.seasonsapp.com/crops/categories/1/?format=json')
-  .then(data => {
-    console.log(data);
-  });
+function FeaturedCtrl(Allotments, filterFilter, $scope, $http){
+  const vm = this;
+  vm.allotments = [];
+
+  getAllotments();
+  function getAllotments() {
+    $http.get('http://localhost:7000/api/allotments')
+    .then((res) => {
+      vm.allotments = res.data[0].result.records;
+      console.log(vm.allotments);
+      startWatch();
+    });
+  }
+
+  function filterAllotments() {
+    const params = {
+      Name: vm.q,
+      NearestPostcode: vm.NearestPostcode
+    };
+    vm.allotmentsFiltered = filterFilter(vm.allotments, params);
+    // console.log(vm.allotmentsFiltered);
+  }
+
+  function startWatch() {
+    $scope.$watchGroup([
+      () => vm.q,
+      () => vm.NearestPostcode
+      // () => vm.result.records.Name
+    ], filterAllotments);
+  }
 }
-
-
-
-// const cat = 5;
-// for(var i = 0; i < cat; i++) {
-//   $http
-//   .get(`http://api.seasonsapp.com/crops/categories/${i}/?format=json`)
-//   .$promise
-//   .then(data => {
-//
-//   })
-//
-// }
