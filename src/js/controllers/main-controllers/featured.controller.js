45f85c5b5c0c1ad82bea24f83ea2f64454feed0d
catley85 <chriscatley85@gmail.com>
2017-08-14 22:50:12 +0100

    Google distance basics there but not functioning

--- src/js/controllers/main-controllers/featured.controller.js
+++ src/js/controllers/main-controllers/featured.controller.js
@@ -2,23 +2,46 @@ angular
 .module('WDI_Group_Project')
 .controller('FeaturedCtrl', FeaturedCtrl);
 
-FeaturedCtrl.$inject = ['Allotments','filterFilter', '$scope', '$http'];
-function FeaturedCtrl(Allotments, filterFilter, $scope, $http){
+FeaturedCtrl.$inject = ['Allotments','filterFilter', '$scope', '$http', 'CurrentUserService'];
+function FeaturedCtrl(Allotments, filterFilter, $scope, $http, CurrentUserService){
   const vm = this;
   vm.allotments = [];
   vm.crops = [];
+  vm.user = CurrentUserService.currentUser;
+  vm.destination = 'ec1y4ab';
+
+  // $http
+  // .get(`http://maps.googleapis.com/maps/api/distancematrix/json?origins=${vm.user.firstName}&destinations=${vm.destination}&mode=walking&units=imperial&key=AIzaSyA8sivM6qLlU7ByUEsaQ0gxDBGtYYCtCXc`)
+  // .then(data => {
+  //   console.log(data);
+  // });
+
+  // const origin = vm.user.firstName;
+  // const destination = vm.destination;
+  //
+  // const service = new google.maps.DistanceMatrixService();
+  // service.getDistanceMatrix(
+  //   {
+  //     origins: [origin],
+  //     destinations: [destination],
+  //     travelMode: 'DRIVING'
+  //   }, getDistance);
+  //
+  // function getDistance(response) {
+  //   console.log(response);
+  // }
+
 
   getAllotments();
   getCrops();
 
+  ///////// GET ALLOTMENTS AND ADD WATCH FOR USER INPUT /////////
   function getAllotments() {
-    if() {
-      $http.get('http://localhost:7000/api/allotments')
-      .then((res) => {
-        vm.allotments = res.data[0].result.records;
-        startWatch();
-      });
-    }
+    $http.get('http://localhost:7000/api/allotments')
+    .then((res) => {
+      vm.allotments = res.data[0].result.records;
+      startWatch();
+    });
   }
 
   function filterAllotments() {
@@ -33,8 +56,7 @@ function FeaturedCtrl(Allotments, filterFilter, $scope, $http){
     ], filterAllotments);
   }
 
-
-
+//////////// GET CROPS AND ADD WATCH FOR USER INPUT ////////////
   function getCrops() {
     $http.get('http://localhost:7000/api/crops')
     .then((res) => {
@@ -47,12 +69,10 @@ function FeaturedCtrl(Allotments, filterFilter, $scope, $http){
       name: vm.cropName
     };
     vm.cropsFiltered = filterFilter(vm.crops, params);
-    console.log(vm.cropsFiltered);
   }
   function startCropWatch() {
     $scope.$watchGroup([
       () => vm.cropName
-      // () => vm.result.records.Name
     ], filterCrops);
   }
 }
